80e92ce47f56e46bd779d50608c909a617b57971
Muhammad Iqbal Septian <yakuzuyoa@gmail.com>
2024-02-16 21:33:08 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module encrypted-database
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+type Employee struct {
+	Id            string
+	Name          string
+	DOB           string
+	Sex           string
+	Address       string
+	Religion      string
+	MarriedStatus string
+	Work          string
+	BloodType     string
+}
+
+func main() {
+
+}
